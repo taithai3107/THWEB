e024cf3ea87d6ff6f743453648ea3ba9e18000d0
= <=>
2020-12-12 22:15:59 +0700

    here

---
+++ newsportal/admin/includes/footer.php
@@ -0,0 +1,4 @@
+
+                <footer class="footer text-right">
+                   Không có ai đâu =))).
+                </footer>
---
+++ newsportal/includes/footer.php
@@ -0,0 +1,6 @@
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; TÀI ĐẸP TRAI</p>
+      </div>
+      <!-- /.container -->
+    </footer>
